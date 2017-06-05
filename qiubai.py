3de1a4d265f93ee62b1bc0955a3cc7ad2c31995e
ilov4tx <25331078@qq.com>
2017-06-06 00:25:11 +0800

    v2

--- qiubai.py
+++ qiubai.py
@@ -1,10 +1,20 @@
-
-
 import urllib.request
+import re
 
 url = 'https://www.qiushibaike.com/'
-try
-req = urllib.request.Request(url)
+user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64)'
+headers = {'User-Agent':user_agent}
+try:
+    request = urllib.request.Request(url,headers=headers)
+    respone = urllib.request.urlopen(request)
+    print(respone.read())
 
 except urllib.request.URLError or e:
+    if hasattr(e,'code'):
+        print(e.code)
+    if hasattr(e,'reason'):
+        print(e.reason)
+
+content = urllib.request.respone.read().decode('utf-8')
+pattern = re.compile()
 
